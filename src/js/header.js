271a83046d485a8c7c4e73bec075784f23c8ba1f
lanbosm <lanbosm@gmail.com>
2017-02-13 00:54:46 +0800

    22

--- src/js/header.js
+++ src/js/header.js
@@ -2,36 +2,18 @@
 import util from 'util/util.js';
 
 !(function (){
-	var templates=''+
-			'<div class="navbar navbar-inverse navbar-fixed-top" id="header">'+
-				 '<a class="navbar-brand"><img src="/images/logo.jpg" alt="Brand"></a>'+
-				'<div class="container">'+
-					'<div class="staff">{staff}</div>'+
-			       ' <div class="navbar-header">'+
-			          '<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse">'+
-			            '<span class="sr-only">Toggle navigation</span>'+
-			            '<span class="icon-bar"></span>'+
-			            '<span class="icon-bar"></span>'+
-			            '<span class="icon-bar"></span>'+
-			          '</button>'+
-			        '</div>'+
-			        '<div class="navbar-collapse collapse" role="navigation">'+
-			          '<ul class="nav navbar-nav">'+
-			          	'{custom}'+
-			            '<li><a class="custom-add" onclick="queueTools.addCustom();">+</a></li>'+
-			            '<li><a class="custom-remove" onclick="queueTools.removeCustom();">-</a></li>'+
-			          '</ul>'+
-			       ' </div>'+
-			 	'</div>'+
-   	   		'</div>';
-		
+
+	if(!document.getElementById("header-nav")){return false;}
+    var navbar=document.getElementById("header-nav");
+	var template=navbar.innerHTML;
+
 
 	var queueTools={
 		login:false,
-		template: templates,
+		template: template,
 		data:{
 			index:0,
-			staff:"收营员XXX",
+			staff:"里可行",
 			list:[
 	 			
 	 		]
@@ -39,19 +21,25 @@ import util from 'util/util.js';
 		init:function(){
 			
 			var localData=util.pullLocal("queue");
-			if(localData){this.data=localData;}			
+			if(localData){this.data=localData;}
+
+			if(this.data.list==0){
+				this.addCustom();
+			}
+
+           this.refresh();
+			this.contact();
 		},
-		getSystemCustom:function(){
-			//默认匿名用户
+        //获得用户资料
+		getCustomData:function(){
 			var custom={}
+            custom.uid="1001";
 			custom.name="custom_"+new Date().getTime();
-			custom.cid=null;
-
-			
-
+			custom.cid=parseInt(Math.random()*1000);
 			return custom;
 		},
-		getSystemTime:function(){
+        //获得时间信息
+		getTimeData:function(){
 			function nz(n){
 					if(n<10){
 						n="0"+n;
@@ -63,18 +51,16 @@ import util from 'util/util.js';
 
 			var time=new Date();
 			var timestr=nz(time.getHours())+":"+nz(time.getMinutes());
+
 			return timestr;
 		},
 		refresh:function(){
 			
 			var html=this.getHeaderHtml();
-			if(!document.getElementById("header")){
-				$("body").append(html);	
-			}
-			else{
-				document.getElementById("header").innerHTML=html;			
-			}
-   	 		//pushLocal("miku",this.data);
+
+			navbar.innerHTML=html;
+
+
 		},
 		makeCustom:function(data){
 			var _self=this;
@@ -90,28 +76,33 @@ import util from 'util/util.js';
 			})  
 			return html;
 		},
+		//获得最新的html
 		getHeaderHtml:function(){
 			var staff=this.data.staff;
-			var queue=this.data.list;
-			var custom=this.makeCustom(queue);
+			var aaa=this.data.list;
+			var queue=this.makeCustom(aaa);
 			var tmp = this.template.replace(/{staff}/g, staff)
-			.replace(/{custom}/g, custom);
+			.replace(/{queue}/g, queue);
 			return tmp;
 		},
+		//切换用户
 		switchCustom:function(index){
 			this.data.index=index-1;
 			this.refresh();
             util.pushLocal("queue",this.data);
 
-			//alert(queueTools.data.list[queueTools.data.index].custom.name)
+            //查询购物车
+			this.findCart(this.data.list[index-1].custom.name,this.data.list[index-1].custom.cid)
+
 		},
-		addCustom:function(custom){
+		//新增用户
+		addCustom:function(){
 			var queue={};
 			//记录客户当前时间
-			queue.time=this.getSystemTime();
+			queue.time=this.getTimeData();
 			//记录客户名会员
-			queue.custom=this.getSystemCustom()
-			
+			queue.custom=this.getCustomData()
+			//
 			this.data.list.push(queue);
 			this.data.index=this.data.list.length-1;
 
@@ -119,27 +110,65 @@ import util from 'util/util.js';
             util.pushLocal("queue",this.data);
 		
 		},
-		setCustom:function(custom){
-			if(this.data.list.length==0){return ;}
-	 		var index =this.data.index;
-	 		alert(index);
-	 		this.data.list[index].custom=custom;
-            util.pushLocal("queue",this.data);
-		
-		},
+
+		//删除用户
 		removeCustom:function(){
 			var index=this.data.index;
 			this.data.list.splice(index,1);
 			this.refresh();
             util.pushLocal("queue",this.data);
 
-		}
+		},
+		//查询购物记录
+		findCart:function(name,cartid){
+			alert("用户名:"+name+",购物车id:"+cartid)
+
+		},
+        //设置用户信息
+        setCustom:function(custom){
+            if(this.data.list.length==0){return ;}
+            var index =this.data.index;
+
+            this.data.list[index].custom=custom;
+            util.pushLocal("queue",this.data);
+
+        },
+        //连接设备
+        contact:function(){
+            document.querySelector('.contact-btn').classList.remove('success','error');
+          if(2>1) {
+              document.querySelector('.contact-btn').classList.add('success');
+
+          }else{
+              document.querySelector('.contact-btn').classList.add('error');
+		  }
+
+        },
+        //连接设备
+        exit:function(){
+        	var res=confirm("确定退出吗? ");
+        	if(res) {
+                util.delLocal('queue');
+                location.href="./login.html";
+            }
+
+        },
+        //设置用户信息
+        setCustom:function(custom){
+            if(this.data.list.length==0){return ;}
+            var index =this.data.index;
+
+            this.data.list[index].custom=custom;
+            util.pushLocal("queue",this.data);
+
+        },
 
 	}
 	   
-//
+
 	queueTools.init();
-	queueTools.refresh();
+
+
 
 	window.queueTools=queueTools;
 
